26f88f9eda47831e7ce2075dca496a5540b09bd3
vinicius <vinicius.devjvm@gmail.com>
2024-01-06 17:44:05 -0300

    Inicio do estudo de funcoes

---
+++ funcoes/class01.js
@@ -0,0 +1,25 @@
+function falaOi() {
+    console.log('Oi')
+}
+
+const souUmDado = function (){
+    console.log('sou um dado')
+}
+
+const arrow = () => {
+    console.log('Arrowwww')
+}
+
+const obj= {
+    falar() {
+        console.log('Estou falando')
+    }
+}
+
+obj.falar();
+
+arrow();
+
+falaOi();
+
+souUmDado();
