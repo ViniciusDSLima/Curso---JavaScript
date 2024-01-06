216ef766b74313d949e884eec7230f8587c6eec4
vinicius <vinicius.devjvm@gmail.com>
2024-01-06 15:24:25 -0300

    Primeira ideia de funcoes

---
+++ fundamentals01/class01.js
@@ -0,0 +1,16 @@
+function saudacao(nome){
+    return `Bom dia! ${nome}`;
+}
+
+const varivel = saudacao('Vinicius');
+console.log(varivel);
+
+
+const somarDoisNumeros = (a,b) => {
+    a + b;
+}
+
+console.log(somarDoisNumeros(1,2))
+const raiz = (n) => n ** 0.5;
+
+console.log(raiz(3))
